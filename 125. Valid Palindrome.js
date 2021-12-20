c1fb1235adb531bb91224e32edef1761a7c1dcab
Toufiq Alahi <nipuntan01@gmail.com>
2021-12-20 20:59:31 +0600

    first commit

---
+++ 125. Valid Palindrome.js
@@ -0,0 +1,13 @@
+function ispalindrome(s) {
+    s = s.toLowerCase().replace(/[\W_]/g, "");
+    let left = 0;
+    let right = s.length - 1;
+    while (left < right) {
+        if(s[left] !== s[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
+}
