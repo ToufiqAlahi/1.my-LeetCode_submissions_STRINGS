4742b568a0013d8d13b2f4f17d96c23b0c0e877b
ToufiqAlahi <nipuntan01@gmail.com>
2021-12-21 17:29:24 +0600

    Update roman to int impl.js

--- roman to int impl.js
+++ roman to int impl.js
@@ -3,10 +3,38 @@
  * @return {number}
  */
 var romanToInt = function (s) {
-    const roman = [I, V, X, L, C, D, M];
-    const int = [1, 5, 10, 50, 100, 500, 1000];
-    roman[] = int[];
+    let romanToInt = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    };
+    let total = 0;
+    for (let i = 0; i < s.length; i++) {
+        let currentInt = romanToInt[s.charAt(i)];
+        let nextInt = romanToInt[s.charAt(i + 1)];
 
+        if (nextInt) {
+            if (currentInt >= nextInt) {
+                total += currentInt;
 
+            } else {
+                total += (nextInt - currentInt);
+                i++;
 
+            }
+
+        }else{
+            total += currentInt;
+
+        }
+
+    }
+    return total;
 };
+
+var result = romanToInt("MCMXCVI");
+console.log(result);
